2703240960ad81bd5f2eb40d71d50b50041832f7
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2022-09-14 14:08:47 +0200

    Create 09-14.py
    14/09/22

---
+++ 09-14.py
@@ -0,0 +1,45 @@
+from codecs import xmlcharrefreplace_errors
+from tkinter import Y
+
+
+print('Szia uram')
+# ez egy comment    
+"""
+ez
+egy 
+többsoros 
+megjegyzés
+"""
+#VÁLTOZÓK
+""""
+- egész számok
+- lebegőpontos számok
+- sztringek
+
+"""
+x = 5
+print(type(x))
+#Típusát leírja
+y = 3.14
+print(type(y))
+s = "kék az ég"
+print(type(s))  
+
+print(x)
+print(y)
+print(s)
+
+#Változók elnevezésénk szabályai:
+"""
+- A változó neve mindig kisbetűvel kezd
+- A változó neve nem tartalmazhat ékezetes betűt
+- A változó neve nem kezdődhet számmal
+- A változó neve csak kis- és nagybetűket, valamint számokat és aláhúzás(_)karaktert tartalmazhat
+- A változók neve case-sensitive(megkülönbözteti a kis&nagy betűket!!!, betűérzékeny) 
+"""
+
+# Néhány példa: 
+myvar = "Pista"
+my_var = "Pista"
+myVar = "Pista"
+myVar2 = "Béla"
